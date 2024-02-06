d9981d0e15ec8bf04a508e3b433e215866aae835
Amulyadharanesh <amulyadharaneshxworkz.gmail.com>
2024-02-06 12:59:11 +0530

    override

--- override/src/com/xworkz/override/runner/FoodRunner.java
+++ override/src/com/xworkz/override/runner/FoodRunner.java
@@ -8,7 +8,7 @@ public class FoodRunner {
 
 	public static void main(String[] args) {
 		
-		 Food food = new Food();
+		    Food food = new Food();
 	        food.eat();
 	        food.digest();
 	        food.prepare();
@@ -19,12 +19,39 @@ public class FoodRunner {
 	        fruit.peel();
 	        fruit.prepare();
 	        
+	        Food food1=new Fruit();
+	        food1.eat();
+	        food1.digest();
+	        food1.prepare();
+	        food1.digest();
+	        
+	        
 	        Orange orange=new Orange();
 	        orange.digest();
 	        orange.eat();
 	        orange.peel();
 	        orange.prepare();
 	        orange.juice();
+	        
+	        Food orange1=new Orange();
+	        orange1.eat();
+	        orange1.digest();
+	        orange1.prepare();
+	        orange1.digest();
+	        
+	        Fruit orange2=new Orange();
+	        orange2.digest();
+	        orange2.eat();
+	        orange2.peel();
+	        orange2.prepare();
+	        
+	        Orange fru =(Orange)orange;
+	        fru.juice();
+	        
+	        Food foo=(Food)food1;
+	        foo.digest();
+	        
+	        
 
 
 	}
